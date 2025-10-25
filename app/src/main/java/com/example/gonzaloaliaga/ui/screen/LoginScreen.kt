348ceeb2689002f5e5a0adb05d1d5e8615a6c063
Gonzalo Aliaga <gonz.aliaga@duocuc.cl>
2025-10-24 23:32:50 -0300

    mejor navegacion y mejoras visuales de error

--- app/src/main/java/com/example/gonzaloaliaga/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/gonzaloaliaga/ui/screen/LoginScreen.kt
@@ -1,15 +1,23 @@
 package com.example.gonzaloaliaga.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -17,33 +25,63 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.gonzaloaliaga.ui.UsuarioViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(uservm: UsuarioViewModel, navController: NavController, onLoginSuccess: () -> Unit) {
     val form by uservm.form.collectAsState()
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        TextField(
-            value = form.nombre,
-            onValueChange = { uservm.onNombreChange(it) },
-            label = { Text("Nombre") }
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
-            value = form.password,
-            onValueChange = { uservm.onPasswordChange(it) },
-            label = { Text("Contraseña") },
-            visualTransformation = PasswordVisualTransformation()
-        )
-        form.error?.let { Text(it, color = Color.Red) }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { navController.navigate("register") }) {
-            Text("Registrar")
+    Scaffold (
+        topBar = {
+            TopAppBar(title = { Text("Iniciar sesión")})
         }
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
 
-        Button(onClick = { uservm.login(onLoginSuccess) }) {
-            Text("Ingresar")
+            TextField(
+                value = form.nombre,
+                onValueChange = { uservm.onNombreChange(it) },
+                label = { Text("Nombre") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            TextField(
+                value = form.password,
+                onValueChange = { uservm.onPasswordChange(it) },
+                label = { Text("Contraseña") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            form.error?.let { Text(it, color = Color.Red) }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(
+                onClick = { uservm.login(onLoginSuccess) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Ingresar")
+            }
+
+            TextButton(
+                onClick = {
+                    uservm.limpiarError()
+                    navController.navigate("register")
+                }) {
+                Text("¿No tienes una cuenta? ¡Da click aquí!")
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/gonzaloaliaga/ui/screen/RegisterScreen.kt
+++ app/src/main/java/com/example/gonzaloaliaga/ui/screen/RegisterScreen.kt
@@ -1,10 +1,12 @@
 package com.example.gonzaloaliaga.ui.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -21,12 +23,12 @@ fun RegisterScreen(
     // Si hay un mensaje de error en el formulario, lo mostramos
     val error = form.error
 
-    Scaffold(
+    Scaffold (
         topBar = {
             TopAppBar(title = { Text("Registrar Usuario") })
         }
     ) { innerPadding ->
-        Column(
+        Column (
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(16.dp)
@@ -57,9 +59,11 @@ fun RegisterScreen(
             TextField(
                 value = form.rol,
                 onValueChange = { uservm.onRolChange(it) },
-                label = { Text("Rol (por ejemplo: cliente, admin)") },
+                label = { Text("Rol (cliente, admin)") },
                 modifier = Modifier.fillMaxWidth()
             )
+
+            Spacer(modifier = Modifier.height(5.dp))
 
             // Mostrar error si existe
             error?.let {
@@ -67,12 +71,15 @@ fun RegisterScreen(
                 Text(text = it, color = MaterialTheme.colorScheme.error)
             }
 
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(12.dp))
 
+            val context = LocalContext.current
             Button(
                 onClick = {
                     uservm.guardar {
                         // Si el registro fue exitoso, vuelve al login
+                        Toast.makeText(context, "¡Usuario registrado! Inicia sesión para ingresar a la aplicación.", Toast.LENGTH_SHORT).show()
+                        uservm.limpiarError()
                         navController.popBackStack()
                     }
                 },
@@ -81,9 +88,11 @@ fun RegisterScreen(
                 Text("Registrar")
             }
 
-            Spacer(modifier = Modifier.height(12.dp))
-
-            TextButton(onClick = { navController.popBackStack() }) {
+            TextButton(
+                onClick = {
+                    uservm.limpiarError()
+                    navController.popBackStack()
+                }) {
                 Text("Volver al inicio de sesión")
             }
         }
